ff2eaa8d61d1b9bd669d4685bc1d9670bc128400
Wail0s <wail120@live.fr>
2023-02-15 13:26:11 +0100

    import

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script >
+import NavbarComponent from './components/NavbarComponent.vue'
+export default{
+  components:{
+    NavbarComponent
+  }
+}
+</script>
+
+<template>
+
+
+  <div class="mainbox">
+    <NavbarComponent class="navbar"></NavbarComponent>
+  </div>
+
+</template>
+
+<style>
+.navbar{
+  background-color: lightblue;
+  position: fixed;
+  top : 0px;
+  padding: 0;
+  margin: 0;
+
+
+}
+</style>
