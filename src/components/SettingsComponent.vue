a5cdc6e2c55ee8bf243d5d50387c0319deeb4cb7
Wail <wail120@live.fr>
2023-03-01 18:08:21 +0100

    added temperature radio buttons

--- src/components/SettingsComponent.vue
+++ src/components/SettingsComponent.vue
@@ -4,6 +4,11 @@ export default {
   name: "SettingsComponent",
   components:{
     NavbarComponent,
+  },
+  data(){
+    return{
+      temperature_unit : ""
+    }
   }
   
 }
@@ -16,6 +21,21 @@ export default {
   <input type="checkbox" class="custom-control-input" id="customSwitch1">
   <label class="custom-control-label" for="customSwitch1">Toggle this switch element</label>
 </div>
+
+<div class="form-check">
+  <input v-model="temperature_unit" class="form-check-input" type="radio" name="temperature_unit" id="temperature_unit_c" value="C">
+  <label class="form-check-label" for="temperature_unit_c">
+    Celsius (°C)
+  </label>
+</div>
+
+<div class="form-check">
+  <input v-model="temperature_unit" class="form-check-input" type="radio" name="temperature_unit" id="temperature_unit_f" value="F" checked>
+  <label class="form-check-label" for="temperature_unit_f">
+    Fahrenheit (°F)
+  </label>
+</div>
+<h1>{{ temperature_unit }}</h1>
 
 
 </template>
